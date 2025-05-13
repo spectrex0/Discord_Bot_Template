a7b2b3484b28d7c5f48f5805bd3966e3da2b6bc7
spectre <alexandertejedadlr@gmail.com>
2025-05-13 08:51:33 -0400

    First Commit

---
+++ Bot/index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// TEMPLATE FILE
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login('your-bot-token-here');
+
+
+// THIS IS A SIMPLE TEMPLATE THAT REPLY TO PING COMMAND
